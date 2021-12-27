fdacd2788f3b0f25bf490ba41af6ba3695fb88d8
nhardbalansag <nhardbalansag@gmail.com>
2021-12-27 23:38:23 +0800

    modified welcome.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -25,22 +25,33 @@
                         <div class="card">
                             <div class="card-body login-card-body">
                                 <p class="login-box-msg">Sign in to start your session</p>
-                                <form action="#" method="post">
+                                <form method="POST" action="{{ route('login') }}">
+                                    @csrf
                                     <div class="mb-3 input-group">
-                                        <input type="email" class="form-control" placeholder="Email">
+                                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
                                         <div class="input-group-append">
                                             <div class="input-group-text">
                                                 <span class="fas fa-envelope"></span>
                                             </div>
                                         </div>
+                                        @error('email')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
                                     </div>
                                     <div class="mb-3 input-group">
-                                        <input type="password" class="form-control" placeholder="Password">
+                                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
                                         <div class="input-group-append">
                                             <div class="input-group-text">
                                                 <span class="fas fa-lock"></span>
                                             </div>
                                         </div>
+                                        @error('password')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
                                     </div>
                                     <div class="row">
                                         <div class="col-4">
